189636d805e6c80115aa0ef09e27765d32900920
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-05-02 21:23:46 +0530

    Created Tasks Schema and Model

---
+++ task-manager/backend/model/taskModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
