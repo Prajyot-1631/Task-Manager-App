7f33a02ddac3662b9892331f7a1a22102b927a64
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-05-09 01:40:36 +0530

    App.jsx

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
