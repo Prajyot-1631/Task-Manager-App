268e379aaf41f917c8556d4b7b766458893152a7
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-05-09 01:25:23 +0530

    Fixed Deployed Link Error

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
